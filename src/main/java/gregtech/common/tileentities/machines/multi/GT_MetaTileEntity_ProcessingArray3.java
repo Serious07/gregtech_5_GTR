f03d1d393b507441422374a48d5a5002103bad35
Serious07 <Serious-07@inbox.ru>
2019-09-25 02:14:39 +0700

    Добавил базовый класс для Procession Array, добавил в него машину WireAssembler

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_ProcessingArray3.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_ProcessingArray3.java
@@ -30,7 +30,7 @@ import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.FluidStack;
 
-public class GT_MetaTileEntity_ProcessingArray3 extends GT_MetaTileEntity_MultiBlockBase {
+public class GT_MetaTileEntity_ProcessingArray3 extends GT_MetaTileEntity_ProcessingArray_Base {
 
     private GT_Recipe mLastRecipe;
     private int tTier = 0;
@@ -47,7 +47,8 @@ public class GT_MetaTileEntity_ProcessingArray3 extends GT_MetaTileEntity_MultiB
     public IMetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
         return new GT_MetaTileEntity_ProcessingArray3(this.mName);
     }
-
+    
+    @Override
     public String[] getDescription() {
         return new String[]{
                 "Controller Block for the Processing Array Mk 3",
@@ -62,135 +63,8 @@ public class GT_MetaTileEntity_ProcessingArray3 extends GT_MetaTileEntity_MultiB
                 "Maximal overclockedness of machines inside: Tier 11"};
     }
 
-    public ITexture[] getTexture(IGregTechTileEntity aBaseMetaTileEntity, byte aSide, byte aFacing, byte aColorIndex, boolean aActive, boolean aRedstone) {
-        if (aSide == aFacing) {
-            return new ITexture[]{Textures.BlockIcons.CASING_BLOCKS[48], new GT_RenderedTexture(aActive ? Textures.BlockIcons.OVERLAY_FRONT_PROCESSING_ARRAY_ACTIVE : Textures.BlockIcons.OVERLAY_FRONT_PROCESSING_ARRAY)};
-        }
-        return new ITexture[]{Textures.BlockIcons.CASING_BLOCKS[48]};
-    }
-
-    public Object getClientGUI(int aID, InventoryPlayer aPlayerInventory, IGregTechTileEntity aBaseMetaTileEntity) {
-        return new GT_GUIContainer_MultiMachine(aPlayerInventory, aBaseMetaTileEntity, getLocalName(), "ProcessingArray.png");
-    }
-
-    //TODO: Expand so it also does the non recipe map recipes
-    /*
-    public void remoteRecipeCheck() {
-        if (mInventory[1] == null) return;
-        String tmp = mInventory[1].getUnlocalizedName().replaceAll("gt.blockmachines.basicmachine.", "");
-        if (tmp.startsWith("replicator")) {
-        } else if (tmp.startsWith("brewery")) {
-        } else if (tmp.startsWith("packer")) {
-        } else if (tmp.startsWith("printer")) {
-        } else if (tmp.startsWith("disassembler")) {
-        } else if (tmp.startsWith("massfab")) {
-        } else if (tmp.startsWith("scanner")) {
-        }
-    }
-    */
-
-    public GT_Recipe.GT_Recipe_Map getRecipeMap() {
-        if (mInventory[1] == null) return null;
-        String tmp = mInventory[1].getUnlocalizedName().replaceAll("gt\\.blockmachines\\.basicmachine\\.", "");
-        if (tmp.startsWith("centrifuge")) {
-            return GT_Recipe.GT_Recipe_Map.sCentrifugeRecipes;
-        } else if (tmp.startsWith("electrolyzer")) {
-            return GT_Recipe.GT_Recipe_Map.sElectrolyzerRecipes;
-        } else if (tmp.startsWith("alloysmelter")) {
-            return GT_Recipe.GT_Recipe_Map.sAlloySmelterRecipes;
-        } else if (tmp.startsWith("assembler")) {
-            return GT_Recipe.GT_Recipe_Map.sAssemblerRecipes;
-        } else if (tmp.startsWith("compressor")) {
-            return GT_Recipe.GT_Recipe_Map.sCompressorRecipes;
-        } else if (tmp.startsWith("extractor")) {
-            return GT_Recipe.GT_Recipe_Map.sExtractorRecipes;
-        } else if (tmp.startsWith("macerator")) {
-            return GT_Recipe.GT_Recipe_Map.sMaceratorRecipes;
-        } else if (tmp.startsWith("recycler")) {
-            return GT_Recipe.GT_Recipe_Map.sRecyclerRecipes;
-        } else if (tmp.startsWith("thermalcentrifuge")) {
-            return GT_Recipe.GT_Recipe_Map.sThermalCentrifugeRecipes;
-        } else if (tmp.startsWith("orewasher")) {
-            return GT_Recipe.GT_Recipe_Map.sOreWasherRecipes;
-        } else if (tmp.startsWith("chemicalreactor")) {
-            return GT_Recipe.GT_Recipe_Map.sChemicalRecipes;
-        } else if (tmp.startsWith("chemicalbath")) {
-            return GT_Recipe.GT_Recipe_Map.sChemicalBathRecipes;
-        } else if (tmp.startsWith("electromagneticseparator")) {
-            return GT_Recipe.GT_Recipe_Map.sElectroMagneticSeparatorRecipes;
-        } else if (tmp.startsWith("autoclave")) {
-            return GT_Recipe.GT_Recipe_Map.sAutoclaveRecipes;
-        } else if (tmp.startsWith("mixer")) {
-            return GT_Recipe.GT_Recipe_Map.sMixerRecipes;
-        } else if (tmp.startsWith("hammer")) {
-            return GT_Recipe.GT_Recipe_Map.sHammerRecipes;
-        } else if (tmp.startsWith("sifter")) {
-            return GT_Recipe.GT_Recipe_Map.sSifterRecipes;
-        } else if (tmp.startsWith("extruder")) {
-            return GT_Recipe.GT_Recipe_Map.sExtruderRecipes;
-        } else if (tmp.startsWith("laserengraver")) {
-            return GT_Recipe.GT_Recipe_Map.sLaserEngraverRecipes;
-        } else if (tmp.startsWith("bender")) {
-            return GT_Recipe.GT_Recipe_Map.sBenderRecipes;
-        } else if (tmp.startsWith("wiremill")) {
-            return GT_Recipe.GT_Recipe_Map.sWiremillRecipes;
-        } else if (tmp.startsWith("arcfurnace")) {
-            return GT_Recipe.GT_Recipe_Map.sArcFurnaceRecipes;
-        } else if (tmp.startsWith("brewery")) {
-            return GT_Recipe.GT_Recipe_Map.sBrewingRecipes;
-        } else if (tmp.startsWith("canner")) {
-            return GT_Recipe.GT_Recipe_Map.sCannerRecipes;
-        } else if (tmp.startsWith("cutter")) {
-            return GT_Recipe.GT_Recipe_Map.sCutterRecipes;
-        } else if (tmp.startsWith("fermenter")) {
-            return GT_Recipe.GT_Recipe_Map.sFermentingRecipes;
-        } else if (tmp.startsWith("fluidextractor")) {
-            return GT_Recipe.GT_Recipe_Map.sFluidExtractionRecipes;
-        } else if (tmp.startsWith("fluidsolidifier")) {
-            return GT_Recipe.GT_Recipe_Map.sFluidSolidficationRecipes;
-        } else if (tmp.startsWith("lathe")) {
-            return GT_Recipe.GT_Recipe_Map.sLatheRecipes;
-        } else if (tmp.startsWith("boxinator")) {
-            return GT_Recipe.GT_Recipe_Map.sBoxinatorRecipes;
-        } else if (tmp.startsWith("unboxinator")) {
-            return GT_Recipe.GT_Recipe_Map.sUnboxinatorRecipes;
-        } else if (tmp.startsWith("polarizer")) {
-            return GT_Recipe.GT_Recipe_Map.sPolarizerRecipes;
-        } else if(tmp.startsWith("press")){
-            return GT_Recipe.GT_Recipe_Map.sPressRecipes;
-        } else if (tmp.startsWith("plasmaarcfurnace")) {
-            return GT_Recipe.GT_Recipe_Map.sPlasmaArcFurnaceRecipes;
-        } else if (tmp.startsWith("printer")) {
-            return GT_Recipe.GT_Recipe_Map.sPrinterRecipes;
-        } else if (tmp.startsWith("fluidcanner")) {
-            return GT_Recipe.GT_Recipe_Map.sFluidCannerRecipes;
-        } else if (tmp.startsWith("fluidheater")) {
-            return GT_Recipe.GT_Recipe_Map.sFluidHeaterRecipes;
-        } else if (tmp.startsWith("distillery")) {
-            return GT_Recipe.GT_Recipe_Map.sDistilleryRecipes;
-        } else if (tmp.startsWith("slicer")) {
-            return GT_Recipe.GT_Recipe_Map.sSlicerRecipes;
-        } else if (tmp.startsWith("amplifier")) {
-            return GT_Recipe.GT_Recipe_Map.sAmplifiers;
-        } else if (tmp.startsWith("circuitassembler")) {
-            return GT_Recipe.GT_Recipe_Map.sCircuitAssemblerRecipes;
-        }
-
-        return null;
-    }
-
-    public boolean isCorrectMachinePart(ItemStack aStack) {
-        if (aStack != null && aStack.getUnlocalizedName().startsWith("gt.blockmachines.basicmachine.")) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean isFacingValid(byte aFacing) {
-        return aFacing > 1;
-    }
-
     private String mMachine = "";
+    @Override
     public boolean checkRecipe(ItemStack aStack) {
         if (!isCorrectMachinePart(mInventory[1])) {
             return false;
@@ -345,91 +219,4 @@ public class GT_MetaTileEntity_ProcessingArray3 extends GT_MetaTileEntity_MultiB
         }
         return false;
     }
-    
-    public static ItemStack[] clean(final ItemStack[] v) {
-        List<ItemStack> list = new ArrayList<ItemStack>(Arrays.asList(v));
-        list.removeAll(Collections.singleton(null));
-        return list.toArray(new ItemStack[list.size()]);
-    }
-
-    public boolean checkMachine(IGregTechTileEntity aBaseMetaTileEntity, ItemStack aStack) {
-        int xDir = ForgeDirection.getOrientation(aBaseMetaTileEntity.getBackFacing()).offsetX;
-        int zDir = ForgeDirection.getOrientation(aBaseMetaTileEntity.getBackFacing()).offsetZ;
-        if (!aBaseMetaTileEntity.getAirOffset(xDir, 0, zDir)) {
-            return false;
-        }
-        int tAmount = 0;
-        for (int i = -1; i < 2; i++) {
-            for (int j = -1; j < 2; j++) {
-                for (int h = -1; h < 2; h++) {
-                    if ((h != 0) || (((xDir + i != 0) || (zDir + j != 0)) && ((i != 0) || (j != 0)))) {
-                        IGregTechTileEntity tTileEntity = aBaseMetaTileEntity.getIGregTechTileEntityOffset(xDir + i, h, zDir + j);
-                        if ((!addMaintenanceToMachineList(tTileEntity, 48)) && (!addInputToMachineList(tTileEntity, 48)) && (!addOutputToMachineList(tTileEntity, 48)) && (!addEnergyInputToMachineList(tTileEntity, 48))) {
-                            if (aBaseMetaTileEntity.getBlockOffset(xDir + i, h, zDir + j) != GregTech_API.sBlockCasings4) {
-                                return false;
-                            }
-                            if (aBaseMetaTileEntity.getMetaIDOffset(xDir + i, h, zDir + j) != 0) {
-                                return false;
-                            }
-                            tAmount++;
-                        }
-                    }
-                }
-            }
-        }
-        return tAmount >= 14;
-    }
-
-    public int getMaxEfficiency(ItemStack aStack) {
-        return 10000;
-    }
-
-    public int getPollutionPerTick(ItemStack aStack) {
-        return 0;
-    }
-
-    public int getDamageToComponent(ItemStack aStack) {
-        return 0;
-    }
-
-    public boolean explodesOnComponentBreak(ItemStack aStack) {
-        return false;
-    }
-
-
-    @Override
-    public String[] getInfoData() {
-        long storedEnergy=0;
-        long maxEnergy=0;
-        for(GT_MetaTileEntity_Hatch_Energy tHatch : mEnergyHatches) {
-            if (isValidMetaTileEntity(tHatch)) {
-                storedEnergy+=tHatch.getBaseMetaTileEntity().getStoredEU();
-                maxEnergy+=tHatch.getBaseMetaTileEntity().getEUCapacity();
-            }
-        }
-
-        return new String[]{
-        		StatCollector.translateToLocal("GT5U.multiblock.Progress")+": "+
-        				EnumChatFormatting.GREEN + Integer.toString(mProgresstime/20) + EnumChatFormatting.RESET +" s / "+
-                        EnumChatFormatting.YELLOW + Integer.toString(mMaxProgresstime/20) + EnumChatFormatting.RESET +" s",
-                StatCollector.translateToLocal("GT5U.multiblock.energy")+": "+
-                		EnumChatFormatting.GREEN + Long.toString(storedEnergy) + EnumChatFormatting.RESET +" EU / "+
-                        EnumChatFormatting.YELLOW + Long.toString(maxEnergy) + EnumChatFormatting.RESET +" EU",
-                StatCollector.translateToLocal("GT5U.multiblock.usage")+": "+
-                        EnumChatFormatting.RED + Integer.toString(-mEUt) + EnumChatFormatting.RESET + " EU/t",
-                StatCollector.translateToLocal("GT5U.multiblock.mei")+": "+
-                        EnumChatFormatting.YELLOW+Long.toString(getMaxInputVoltage())+EnumChatFormatting.RESET+ " EU/t(*2A) "+StatCollector.translateToLocal("GT5U.machines.tier")+": "+
-                        EnumChatFormatting.YELLOW+VN[GT_Utility.getTier(getMaxInputVoltage())]+ EnumChatFormatting.RESET,
-                StatCollector.translateToLocal("GT5U.multiblock.problems")+": "+
-                        EnumChatFormatting.RED+ (getIdealStatus() - getRepairStatus())+EnumChatFormatting.RESET+
-                        " "+StatCollector.translateToLocal("GT5U.multiblock.efficiency")+": "+
-                        EnumChatFormatting.YELLOW+Float.toString(mEfficiency / 100.0F)+EnumChatFormatting.RESET + " %",
-                StatCollector.translateToLocal("GT5U.PA.machinetier")+": "+
-                        EnumChatFormatting.GREEN+tTier+EnumChatFormatting.RESET+
-                        " "+StatCollector.translateToLocal("GT5U.PA.discount")+": "+
-                        EnumChatFormatting.GREEN+(1<<mMult)+EnumChatFormatting.RESET + " x",
-                StatCollector.translateToLocal("GT5U.PA.parallel")+": "+EnumChatFormatting.GREEN+((mInventory[1] != null) ? (mInventory[1].stackSize<<mMult) : 0)+EnumChatFormatting.RESET
-        };
-    }
-
 }
